69e72887075a885277eeb57bcb3e232dc8089b48
Jeff Rossi <rossi.jeff@gmail.com>
2024-01-08 22:17:22 -0500

    welcome

--- components/WelcomeMessage.vue
+++ components/WelcomeMessage.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Welcome message</div>
+  <div>
+    <h2>Welcome</h2>
+    <div class="mb-4">
+      This site is a todo list application. Authentication is used so that each
+      user will only see their own todos.
+    </div>
+    <div>
+      For those who wish to avoid entering any personal information an option to
+      log in as a randomly generated user has been provided.
+    </div>
+  </div>
 </template>
