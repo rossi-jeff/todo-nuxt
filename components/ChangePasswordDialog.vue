79e5a2027d8d9fddfac70c06e432c344dfed81bc
Jeff Rossi <rossi.jeff@gmail.com>
2024-01-09 14:59:51 -0500

    user edit

---
+++ components/ChangePasswordDialog.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="change-password-dialog" class="dialog">
+		<div class="dialog-header">
+			<h2>Change Password</h2>
+		</div>
+		<div class="dialog-body">
+			<div class="field">
+				<label for="old-password">Old Password</label>
+				<input
+					type="password"
+					name="old-password"
+					id="edit-old-password"
+					v-model="pass.old"
+				/>
+			</div>
+			<div class="field">
+				<label for="new-password">New Password</label>
+				<input
+					type="password"
+					name="new-password"
+					id="edit-new-password"
+					v-model="pass.new"
+				/>
+			</div>
+			<div class="field">
+				<label for="password-confirmation">Confirmation</label>
+				<input
+					type="password"
+					name="password-confirmation"
+					id="edit-password-confirmation"
+					v-model="pass.confirm"
+				/>
+			</div>
+		</div>
+		<div class="dialog-buttons">
+			<button @click="cancel">Cancel</button>
+			<button @click="updateUser">Change Password</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(['cancel', 'changePassword'])
+const pass = reactive({ old: '', new: '', confirm: '' })
+
+const cancel = () => {
+	emit('cancel')
+}
+
+const updateUser = () => {
+	if (pass.old && pass.new && pass.new == pass.confirm) {
+		emit('changePassword', {
+			OldPassWord: pass.old,
+			NewPassWord: pass.new,
+			Confirmation: pass.confirm,
+		})
+	}
+}
+</script>
---
+++ components/UserEditDialog.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="edit-user-dialog" class="dialog">
+		<div class="dialog-header">
+			<h2>Edit User</h2>
+		</div>
+		<div class="dialog-body">
+			<div class="field">
+				<label for="user-name">User Name</label>
+				<input
+					type="text"
+					name="user-name"
+					id="edit-user-user-name"
+					v-model="user.UserName"
+				/>
+			</div>
+			<div class="field">
+				<label for="email">Email</label>
+				<input
+					type="email"
+					name="email"
+					id="edit-user-email"
+					v-model="user.Email"
+				/>
+			</div>
+		</div>
+		<div class="dialog-buttons">
+			<button @click="cancel">Cancel</button>
+			<button @click="updateUser">Update User</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import type { User } from '../utils/user.type'
+
+const emit = defineEmits(['cancel', 'updateUser'])
+const props = defineProps<{ user: User }>()
+const user = reactive({
+	UserName: props.user.UserName,
+	Email: props.user.Email,
+})
+
+const cancel = () => {
+	emit('cancel')
+}
+
+const updateUser = () => {
+	const { UserName, Email } = user
+	emit('updateUser', { UserName, Email })
+}
+</script>
